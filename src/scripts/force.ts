2929fcae483d214bbe92f229191a52c0afd48e6c
kagan <intend.on.games+git@gmail.com>
2023-02-20 02:10:05 +0900

    Vue3環境で作成し直し

--- src/scripts/force.ts
+++ src/scripts/force.ts
@@ -10,30 +10,71 @@ import {
 const width = 800;
 const height = 800;
 const nodes = [
-  { index: 0 },
-  { index: 1 },
-  { index: 2 },
-  { index: 3 },
-  { index: 4 },
-  { index: 5 },
+  {}, // group1
+  {}, // group2
+  {}, // item1
+  {},
+  {},
+  {},
+
+  {},
+  {},
+  
+  {}, // 8
+
+  {},
+
+  {},
+
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
 ] satisfies SimulationNodeDatum[];
 const links = [
+  // わっか
   { source: 0, target: 1 },
-  { source: 0, target: 2 },
-  { source: 1, target: 3 },
-  { source: 1, target: 3 },
-  { source: 2, target: 1 },
+  { source: 1, target: 2 },
   { source: 2, target: 3 },
   { source: 3, target: 4 },
   { source: 4, target: 5 },
-  { source: 5, target: 3 },
-] satisfies SimulationLinkDatum<SimulationNodeDatum>[];
+  { source: 5, target: 0 },
+// 所属
+  { source: 8, target: 6 , distance: 0.8},
+  { source: 8, target: 7 , distance: 0.8},
+  { source: 16, target: 7 , distance: 0.8},
+  { source: 16, target: 9 , distance: 0.8},
+  // 壁とくっつける
+  { source: 0, target: 8 },
+  { source: 1, target: 8 },
+  { source: 2, target: 8 },
+  { source: 3, target: 8 },
+  { source: 4, target: 8 },
+  { source: 5, target: 8 },
+  // kabe 2
+  { source: 10, target: 11 },
+  { source: 11, target: 12 },
+  { source: 12, target: 13 },
+  { source: 13, target: 14 },
+  { source: 14, target: 15 },
+  { source: 15, target: 10 },
+  { source: 10, target: 16 },
+  { source: 11, target: 16 },
+  { source: 12, target: 16 },
+  { source: 13, target: 16 },
+  { source: 14, target: 16 },
+  { source: 15, target: 16 },
+  // TODO: 他の壁とは影響を与えないようにしなくてはいけない
+] satisfies (SimulationLinkDatum<SimulationNodeDatum> & {distance?: number})[];
 
 const force = d3
   .forceSimulation(nodes) // NOTE: 実行時、nodesは加工されてデータ型が変わる
-  .force("charge", forceManyBody().strength(-1000).distanceMax(300))
-  .force("link", forceLink(links)) // NOTE: 実行時、linksは加工されてデータ型が変わる
+  .force("charge", forceManyBody().strength(-2000).distanceMax(300))
+  .force("link", forceLink(links).strength(data=> data.distance || 0.2)) // NOTE: 実行時、linksは加工されてデータ型が変わる
   .force("center", forceCenter(width / 2, height / 2));
+  console.log(links)
 type NodeDatum = {
   index: number;
   x: number;
@@ -50,17 +91,23 @@ const svg = d3
   .attr("height", height);
 const link = svg
   .append("g")
-  .attr("stroke", "green")
   .selectAll("line")
   .data(links as unknown as LinkDatum[])
-  .join("line");
+  .join("line")
+  .attr("stroke", "green")
+  // .attr("stroke", (data)=>data.index < 6 ? "green" : "")
 const node = svg
   .append("g")
   .selectAll("circle")
   .data(nodes as unknown as NodeDatum[])
   .join("circle")
   .attr("r", 10);
-
+const label = svg.selectAll('text')
+    .data(nodes  as unknown as NodeDatum[])
+    .join('text')
+    .attr("fill", "red")
+    .attr("text-anchor","middle")
+    .text((data) => data.index);
 force.on("tick", () => {
   link
     .attr("x1", (data) => data.source.x)
@@ -68,22 +115,5 @@ force.on("tick", () => {
     .attr("x2", (data) => data.target.x)
     .attr("y2", (data) => data.target.y);
   node.attr("cx", (data) => data.x).attr("cy", (data) => data.y);
+  label.attr("x", (data) => data.x).attr("y", (data) => data.y);
 });
-
-// nodeのラベル周りの設定
-// const label = svg.selectAll('text')
-//     .data(nodes)
-//     .enter()
-//     .append('text')
-//     .attr({
-//         "text-anchor":"middle",
-//         "fill":"white",
-//         "font-size": "9px"
-//     })
-//     .text(function(data) { return data.label; });
-
-// // labelも追随するように
-// label.attr({
-//     x: function(data) { return data.x;},
-//     y: function(data) { return data.y;}
-// });
